5ffcd52beed237528eeb0f0f0d4b29e1d18d3992
Fabrizio Contu <fab.contu@gmail>
2023-11-07 13:23:43 +0100

    sprint 4

--- dist/index.js
+++ dist/index.js
@@ -9,24 +9,73 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 let jokes = document.getElementById("joke");
+let meteos = document.getElementById("meteo");
+let pattern = document.getElementById("pattern");
+const options = {
+    headers: {
+        Accept: "application/json",
+    },
+};
+const jokeAPIs = [
+    "https://api.chucknorris.io/jokes/random",
+    "https://icanhazdadjoke.com",
+];
+const patternSvg = [
+    "magicpattern-1",
+    "magicpattern-2",
+    "magicpattern-3",
+];
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+let randomAPIIndex = 0;
 function loadJokes() {
     return __awaiter(this, void 0, void 0, function* () {
-        const response = yield fetch("https://icanhazdadjoke.com", {
-            headers: {
-                "Accept": "application/json"
-            }
-        });
+        randomAPIIndex = getRandomInt(2);
+        const currentAPI = jokeAPIs[randomAPIIndex];
+        const response = yield fetch(currentAPI, options);
         const data = yield response.json();
-        console.log(data.joke);
         if (jokes) {
-            jokes.innerHTML = data.joke;
-        }
-        else {
-            console.log("joke not found.");
+            jokes.innerHTML = data.value || data.joke;
         }
     });
 }
 loadJokes();
+let randomPatternIndex = 0;
+function loadPattern() {
+    pattern === null || pattern === void 0 ? void 0 : pattern.classList.remove("magicpattern-1", "magicpattern-2", "magicpattern-3");
+    randomPatternIndex = getRandomInt(3);
+    const currentPattern = patternSvg[randomPatternIndex];
+    pattern === null || pattern === void 0 ? void 0 : pattern.classList.add(currentPattern);
+}
+loadPattern();
+function loadWeather() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const responseW = yield fetch("https://api.open-meteo.com/v1/forecast?latitude=40.4165&longitude=-3.7026&current=temperature_2m&timezone=Europe%2FBerlin&forecast_days=1");
+        const dataW = yield responseW.json();
+        console.log(dataW.current.temperature_2m);
+        if (meteos) {
+            meteos.innerHTML = `${dataW.current.temperature_2m} °C`;
+        }
+    });
+}
+loadWeather();
 let button = document.getElementById("next");
 button === null || button === void 0 ? void 0 : button.addEventListener("click", loadJokes);
+button === null || button === void 0 ? void 0 : button.addEventListener("click", loadPattern);
+const reportAcudits = [{}];
+function clientImpression(id) {
+    reportAcudits.push({
+        joke: jokes === null || jokes === void 0 ? void 0 : jokes.innerHTML,
+        score: id,
+        date: Date(),
+    });
+    console.table(reportAcudits);
+    reportJokes();
+}
+function reportJokes() {
+    const reportJokes = reportAcudits.map((el) => el);
+    console.log(reportJokes);
+}
+reportJokes();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,89 @@
-let jokes = document.getElementById("joke");
+let jokes: HTMLElement | null = document.getElementById("joke");
+let meteos: HTMLElement | null = document.getElementById("meteo");
+let pattern: HTMLElement | null = document.getElementById("pattern");
 
+const options = {
+  headers: {
+    Accept: "application/json",
+  },
+};
+
+const jokeAPIs: string[] = [
+  "https://api.chucknorris.io/jokes/random",
+  "https://icanhazdadjoke.com",
+];
+
+const patternSvg: string[] = [
+  "magicpattern-1",
+  "magicpattern-2",
+  "magicpattern-3",
+];
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
+
+let randomAPIIndex: number = 0;
 
 async function loadJokes() {
-   const response = await fetch("https://icanhazdadjoke.com", {
-     headers: {
-       "Accept": "application/json"
-     }
-   });
-   const data = await response.json();
-   console.log(data.joke);
-   if (jokes) {
-      jokes.innerHTML = data.joke;
-    } else {
-      console.log("joke not found.");
-    }
- }
- 
- loadJokes();
- 
-let button = document.getElementById("next");
+  randomAPIIndex = getRandomInt(2);
+  const currentAPI = jokeAPIs[randomAPIIndex];
+
+  const response = await fetch(currentAPI, options);
+  const data = await response.json();
+
+  if (jokes) {
+    jokes.innerHTML = data.value || data.joke;
+  }
+}
+loadJokes();
+
+let randomPatternIndex: number = 0;
+
+function loadPattern() {
+  pattern?.classList.remove(
+    "magicpattern-1",
+    "magicpattern-2",
+    "magicpattern-3"
+  );
+
+  randomPatternIndex = getRandomInt(3);
+  const currentPattern = patternSvg[randomPatternIndex];
+
+  pattern?.classList.add(currentPattern);
+}
+loadPattern();
+
+async function loadWeather() {
+  const responseW = await fetch(
+    "https://api.open-meteo.com/v1/forecast?latitude=40.4165&longitude=-3.7026&current=temperature_2m&timezone=Europe%2FBerlin&forecast_days=1"
+  );
+  const dataW = await responseW.json();
+  console.log(dataW.current.temperature_2m);
+  if (meteos) {
+    meteos.innerHTML = `${dataW.current.temperature_2m} °C`;
+  }
+}
+loadWeather();
+
+let button = document.getElementById("next") as HTMLElement;
 button?.addEventListener("click", loadJokes);
+button?.addEventListener("click", loadPattern);
+
+const reportAcudits = [{}];
+
+function clientImpression(id: number) {
+  reportAcudits.push({
+    joke: jokes?.innerHTML,
+    score: id,
+    date: Date(),
+  });
+  console.table(reportAcudits);
+  reportJokes();
+}
+
+function reportJokes() {
+  const reportJokes = reportAcudits.map((el) => el);
+  console.log(reportJokes);
+}
+reportJokes();
